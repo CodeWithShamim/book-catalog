7fa97aef4e5653544e84f2c2e1056c27922220e4
codewithshamim <shamimislamonline@gmail.com>
2023-07-14 23:34:54 +0600

    tailwind & daisyui added

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/home";
 import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
